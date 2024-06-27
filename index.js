a6e3c7ebf6873fbb38daa70a5da708aee528398f
o0th <o0th@pm.me>
2024-06-27 15:46:38 +0200

    feat: init

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const token = core.getInput('token');
+const version = core.getInput('version');
+const repository = core.getInput('repository');
+const sha = core.getInput('sha');
+
+const [owner, repo] = repository.split('/');
+const octokit = github.getOctokit(token);
+
+(async () => {
+  const tag = await octokit.rest.git.createTag({
+    owner, repo, tag: `v${version}`, message: '', object: sha
+  });
+
+  console.log(tag);
+
+  const release = await octokit.rest.repos.createRelease({
+    owner, repo, tag_name: tag.tag,
+  });
+
+  console.log(release);
+
+  const asset = await octokit.rest.repos.uploadReleaseAsset({
+    owner, repo, release_id: release.id, name, data
+  });
+
+  console.log(asset);
+})();
